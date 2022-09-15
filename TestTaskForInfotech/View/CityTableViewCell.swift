9969ee2cffc420c4d1ca8d5c3ee6890205c42a2a
khrystyna <khrystynamatasova@gmail.com>
2022-09-15 20:38:36 +0300

    add custom TableViewCell

--- TestTaskForInfotech/View/CityTableViewCell.swift
+++ TestTaskForInfotech/View/CityTableViewCell.swift
@@ -8,16 +8,49 @@
 import UIKit
 
 class CityTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    static let identifier = "CityTableViewCell"
+    
+    private let containerStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let picture: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let cityNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupContainerStackView()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupContainerStackView() {
+        contentView.addSubview(containerStackView)
+        containerStackView.addArrangedSubview(picture)
+        containerStackView.addArrangedSubview(cityNameLabel)
+    }
+    
+    public func configure(image: UIImage, name: String) {
+        picture.image = image
+        cityNameLabel.text = name
+    }
 }
